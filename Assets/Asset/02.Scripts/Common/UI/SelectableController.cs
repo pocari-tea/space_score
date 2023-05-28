f842c387c4e7c053d2e5a141a2816f0eb5c2a217
pocari_tea <ekawn12@naver.com>
2023-05-28 23:52:40 +0900

    - selectable 가로모드 추가
    Signed-off-by: pocari_tea <ekawn12@naver.com>

--- Assets/Asset/02.Scripts/Common/UI/SelectableController.cs
+++ Assets/Asset/02.Scripts/Common/UI/SelectableController.cs
@@ -21,6 +21,8 @@ public class SelectableController : MonoBehaviour
     [SerializeField] private AudioClip confirmAudioClip;
 
     private int currentIndex;
+
+    public bool isHorizon;
     
     UISelect GetCurrentUISelect()
     {
@@ -29,9 +31,17 @@ public class SelectableController : MonoBehaviour
 
     private void ChangeSelection()
     {
-        // x좌표는 화살표 위치에 두고 y좌표만 이동
-        selectIndicator.anchoredPosition = new Vector2(selectIndicator.anchoredPosition.x, selectableOptions[currentIndex].anchoredPosition.y);
-        selectIndicator.anchoredPosition = new Vector2(selectableOptions[currentIndex].anchoredPosition.x, selectIndicator.anchoredPosition.y);
+        if (!isHorizon)
+        {
+            // x좌표는 화살표 위치에 두고 y좌표만 이동
+            selectIndicator.anchoredPosition = new Vector2(selectIndicator.anchoredPosition.x, selectableOptions[currentIndex].anchoredPosition.y);
+        }
+        else
+        {
+            // y좌표는 화살표 위치에 두고 x좌표만 이동
+            selectIndicator.anchoredPosition = new Vector2(selectableOptions[currentIndex].anchoredPosition.x, selectIndicator.anchoredPosition.y);
+        }
+
     }
     
     public void Navigate(InputAction.CallbackContext context)
@@ -41,11 +51,11 @@ public class SelectableController : MonoBehaviour
             float x = context.ReadValue<Vector2>().x;
             float y = context.ReadValue<Vector2>().y;
 
-            if(y == -1 && currentIndex < selectableOptions.Count - 1 || x == -1 && currentIndex < selectableOptions.Count - 1)
+            if(y == -1 && currentIndex < selectableOptions.Count - 1 || x == 1 && currentIndex < selectableOptions.Count - 1)
             {
                 currentIndex += 1;
             }
-            else if(y == 1 && currentIndex > 0 || x == 1 && currentIndex > 0)
+            else if(y == 1 && currentIndex > 0 || x == -1 && currentIndex > 0)
             {
                 currentIndex -= 1;
             }
